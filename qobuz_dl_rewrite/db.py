75c4e53f06207ca2e5cb4b4c38aa7b1fc260993c
nathannathant <74019033+pynathanthomas@users.noreply.github.com>
2021-03-10 09:30:21 -0800

    comments in QobuzDB

--- qobuz_dl_rewrite/db.py
+++ qobuz_dl_rewrite/db.py
@@ -7,10 +7,18 @@ logger = logging.getLogger(__name__)
 
 
 class QobuzDB:
-    def __init__(self, db_path):
+    """Simple interface for the downloaded track database."""
+
+    def __init__(self, db_path: str):
+        """Create a QobuzDB object
+
+        :param db_path: filepath of the database
+        :type db_path: str
+        """
         self.path = db_path
 
     def create(self):
+        """Create a database at `self.path`"""
         with sqlite3.connect(self.path) as conn:
             try:
                 conn.execute("CREATE TABLE downloads (id TEXT UNIQUE NOT NULL);")
@@ -19,13 +27,24 @@ class QobuzDB:
                 pass
             return self.path
 
-    def __contains__(self, item_id):
+    def __contains__(self, item_id: str) -> bool:
+        """Checks whether the database contains an id.
+
+        :param item_id: the id to check
+        :type item_id: str
+        :rtype: bool
+        """
         with sqlite3.connect(self.path) as conn:
             return conn.execute(
                 "SELECT id FROM downloads where id=?", (item_id,)
             ).fetchone()
 
-    def add(self, item_id):
+    def add(self, item_id: str):
+        """Adds an id to the database.
+
+        :param item_id:
+        :type item_id: str
+        """
         with sqlite3.connect(self.path) as conn:
             try:
                 conn.execute(
