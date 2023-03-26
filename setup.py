fb5944a5ed3471002980d78673749d50104d8775
Vitiko <averroista@protonmail.com>
2023-03-26 15:32:44 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setup(
     name=pkg_name,
-    version="0.9.9.9",
+    version="0.9.9.10",
     author="Vitiko",
     author_email="vhnz98@gmail.com",
     description="The complete Lossless and Hi-Res music downloader for Qobuz",
